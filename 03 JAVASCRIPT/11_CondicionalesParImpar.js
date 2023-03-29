e4278faf283e9edbce2ed6cc777c1f3b83e5a870
Esborlan <esborlan.daw@gmail.com>
2023-03-29 10:24:08 +0200

    Cambio HTML

--- 03 JAVASCRIPT/11_CondicionalesParImpar.js
+++
@@ -1,9 +0,0 @@
-let number = Number(prompt("Introduce un número."));
-
-if(number % 2 == 0){
-console.log("El número es par");
-}
-else{
-    console.log("El número es impar");
-}
-
--- 04 TYPESCRIPT/002_Enums.ts
+++
@@ -1,5 +0,0 @@
-
-
-function suma(x:number, y:number): number;
-function suma(x:number, y:number, z:number): number;
-
